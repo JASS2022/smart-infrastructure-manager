817b04da9bc14a4bfa0320d17ca644801680e086
Jessica Saroufim <42101410+jessicasaroufim@users.noreply.github.com>
2022-06-13 13:14:51 +0200

    --

---
+++ Car.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from typing import List
+import datetime
+from enum import Enum
+
+
+class State(Enum):
+    WAITING = "Waiting"
+    MOVING = "Moving"
+
+
+class Car:
+
+    def __init__(self, id, entrance, exit, entrance_time, speed):
+        self.id = id
+        self.entrance = entrance
+        self.exit = exit
+        self.entrance_time = entrance_time
+        self.speed = speed
+        self.inGeofence = datetime.datetime.now()
+        self.path = []
+        self.state = State.WAITING
+
+    def get_waiting_time(self):
+        return datetime.datetime.now() - self.inGeofence
+
+    def update_path(self, path):
+        self.path = path
---
+++ Roundabout.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+class Roundabout:
+    def __init__(self, entrances, exits):
+        self.entrances = entrances
+        self.exits = exits
